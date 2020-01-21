49d878f15ce4e4ca4353add2944da42c03ec33b8
Tanner Kay <tleok100@gmail.com>
2020-01-21 15:52:30 -0700

    finished

--- index.js
+++ index.js
@@ -2,42 +2,40 @@
 
 // put your own value below!
 const apiKey = 'TLcvOko6Ev3dwjjinbQvuqlXoMDN1VIsiQ7e40BD'
-const searchURL = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=TLcvOko6Ev3dwjjinbQvuqlXoMDN1VIsiQ7e40BD'
+const searchURL = 'https://developer.nps.gov/api/v1/parks'
 
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
+  const querydata = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+  return querydata.join('&');
 }
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
   $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
+  // iterate through the data array
+  for (let i = 0; i < responseJson.data.length; i++){
+    // for each video object in the data 
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
     $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a>
       </li>`
     )};
   //display the results section  
   $('#results').removeClass('hidden');
 };
 
-function getYouTubeVideos(query, maxResults=10) {
+function getStateParks(searchState, maxResults) {
   const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
+    api_key: apiKey,
+    stateCode: searchState,
+    limit: maxResults
   };
   const queryString = formatQueryParams(params)
   const url = searchURL + '?' + queryString;
@@ -60,9 +58,9 @@ function getYouTubeVideos(query, maxResults=10) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
+    const searchState = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    getStateParks(searchState, maxResults);
   });
 }
 
